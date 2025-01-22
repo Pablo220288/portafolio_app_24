24e435ee19640444bcd4b6197fd7688707d94d5c
Pablo <peh_tj@hotmail.com>
2025-01-22 09:00:53 -0300

    Add Socials Links

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -10,11 +10,20 @@ import {
 } from "react-icons/ri";
 
 const icons = [
-  { path: "/", name: <RiWhatsappFill /> },
-  { path: "/", name: <RiGoogleFill /> },
-  { path: "/", name: <RiTwitchFill /> },
-  { path: "/", name: <RiYoutubeFill /> },
-  { path: "/", name: <RiLinkedinFill /> },
+  {
+    path: "https://wa.me/5493516342315?text=Hola,%20me%20gustaría%20obtener%20más%20información",
+    name: <RiWhatsappFill />,
+  },
+  {
+    path: "mailto:peh7sep@gmail.com?subject=Consulta%20por%20mi%20Pagina%20Web&body=Hola,%20quisiera%20obtener%20más%20información",
+    name: <RiGoogleFill />,
+  },
+  /* { path: "/", name: <RiTwitchFill /> }, */
+  { path: "https://www.youtube.com/@DevPaul-yb1ci", name: <RiYoutubeFill /> },
+  {
+    path: "https://www.linkedin.com/in/pablo-hern%C3%A1ndez-19234b238/",
+    name: <RiLinkedinFill />,
+  },
 ];
 
 const Socials = ({ containerStyles, iconStyles }) => {
